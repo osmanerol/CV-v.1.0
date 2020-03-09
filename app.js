8ebb8ab3a6a3422e84dea70030dc61229833dfa8
osmanerol <osmanerol98@outlook.com>
2020-03-09 13:29:09 +0300

    CV

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+const indexRoutes = require('./routes/indexRoutes');
+
+// view engine setup
+app.use(express.static('public'));
+app.set('view engine','ejs');
+
+app.use(indexRoutes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
